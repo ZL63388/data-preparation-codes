e6772098328429cb9ec4cf3020c7d9b0a6926f28
ZL63388 <71423442+ZL63388@users.noreply.github.com>
2021-07-13 11:28:05 +0800

    Add files via upload

---
+++ missing_values_with_knnimputer.py
@@ -0,0 +1,28 @@
+# import packages
+import pandas as pd
+import numpy as np
+from sklearn.impute import KNNImputer
+
+# create a sample dataframe
+my_df = pd.DataFrame({"A": [1,2,3,4,5],
+                      "B": [1,1,3,3,4],
+                      "C": [1,2,9,np.nan,20]})
+
+
+# define the package into a variable
+knn_imputer = KNNImputer()
+
+
+# define the package into a variable with a specific parameter 
+# the default n_neighbors is 5
+knn_imputer = KNNImputer(n_neighbors = 1)
+knn_imputer = KNNImputer(n_neighbors = 2)
+
+# define the package into a variable with the weights parameter
+# the default value of weights = uniform
+knn_imputer = KNNImputer(n_neighbors = 2, weights = "distance")
+knn_imputer.fit_transform(my_df)
+
+
+# change the datatype to dataframe 
+my_df1 = pd.DataFrame(knn_imputer.fit_transform(my_df), columns = my_df.columns)
